e3f4f1c08b2a7868d166524f89e5d827698fc0dc
Paulina <p.kijowska@gmail.com>
2019-06-26 09:46:21 +1000

    week01 day02 HW

---
+++ paulina_kijowska/week01/strings/js/strings.js
@@ -0,0 +1,19 @@
+
+
+ const drEvil = function(num){
+if (num != 1000000) {
+return num + " dollars";
+} else {
+  return num + " dollars (pinky)";
+ }
+}
+ drEvil(20000);
+
+
+ const mixUp = function(){
+
+ }
+
+ function mixUp(a, b) {
+  return b.slice(0, 2) + a.slice(2) + " " + a.slice(0, 2) + b.slice(2);
+}
