ed566872692c57e9569a3124b9eadb8925e18799
Pakaros Pumanee <pakarospumanee@Pakaross-MacBook.local>
2019-06-27 15:05:03 +1000

    week01 array HW

--- amy_pumanee/week01/strings/js/strings.js
+++ amy_pumanee/week01/strings/js/strings.js
@@ -13,18 +13,18 @@ Create a function called DrEvil. It should take a single argument, an amount, an
   DrEvil(1000000): 1000000 dollars (pinky)
 ```
 */
-var i = 10;
-var i = 1000000;
-if (i < 1000000) {
-  console.log(`DrEvil ${i} : ${i} dollars`);
+const DrEvil = function(num1){
+  if (num1 < 1000000) {
+    console.log(`DrEvil ${num1} : ${num1} dollars`);
+  }
+  else if (num1 === 1000000) {
+    console.log(`DrEvil ${num1} : ${num1} dollars (pinky)`);
+  }
 }
-else if (i = 1000000) {
-  console.log(`DrEvil ${i} : ${i} dollars (pinky)`);
-}
-
+DrEvil(10)
+DrEvil(1000000)
 /*
 ## MixUp
-
 Create a function called mixUp. It should take in two strings, and return the concatenation of the two strings (separated by a space) slicing out and swapping the first 2 characters of each. You can assume that the strings are at least 2 characters long. For example:
 ```
   mixUp('mix', 'pod'): 'pox mid'
@@ -32,15 +32,22 @@ Create a function called mixUp. It should take in two strings, and return the co
 Look up the JavaScript string reference to find methods which may be useful!
 ```
 */
-  let txt = (mix, pod); {
-  //console.log(`mixUp ${string.5} + ${string.6} + ${string.0}: '`);
-  console.log(`mixUp ${txt.charAt(5)} + ${txt.charAt(6)}`);
-}
+  const mixUp = function (str1, str2) {
+    const result1 = str2.slice(0,2) + str1.slice(2,str1.length);
+    const result2 = str1.slice(0,2) + str2.slice(2,str2.length);
 
+    // console.log(result1);
+    // console.log(result2);
+    //const result2 = str1.slice(0,2) + str2.slice(-2);
+    console.log(`Mix up of ${str1} and ${str2} becomes ${result1} ${result2}`);
+    //return result;
+}
+mixUp('mix', 'pod');
+mixUp('dog', 'dinner');
+mixUp('butter', 'mother');
 
 /*
 ## FixStart
-
 Create a function called fixStart. It should take a single argument, a string,
  and return a version where all occurences of its first character
  have been replaced with '*', except for the first character itself.
@@ -49,7 +56,24 @@ Create a function called fixStart. It should take a single argument, a string,
 ```
 fixStart('babble'): 'ba**le'
 ```
+*/
+const fixStart = function (s) {
+  const c = s.charAt(0);
+  let outputStr = '';
+// literate through the entire string pushing letters into a new string unless it is the first letter
+    for (let i = 0; i < s.length; i++) {
+  // if the letter is the first letter AND we are not checking the first letter
+    if (s[i] === c && i !== 0) {
+    outputStr += '*';
+    } else {
+      outputStr += s[i];
+    }
+return outputStr;
+};
+s = 'babble'
+console.log(fixStart(s));
 
+/*
 ## Verbing
 
 Create a function called verbing. It should take a single argument, a string. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. For example:
