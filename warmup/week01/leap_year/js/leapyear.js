ff87913bea9e70f07e55bc8d545f6e9d32534bfc
RashidaBengali <bengali.rashida@gmail.com>
2019-06-28 10:21:52 +1000

    updated leapyer solution

--- warmup/week01/leap_year/js/leapyear.js
+++ warmup/week01/leap_year/js/leapyear.js
@@ -9,25 +9,25 @@ const leapYear = function (year) {
         return true;
       }
         return false;
-      //}
     }
     return true;
   }
     return false;
-  //}
 };
 */
 
 //Version 2
 
 const leapYear = function (year) {
-  if ((year % 4 === 0) && (year % 100 === 0) || (year % 400 === 0)) {
-    return true;
-  }
-  return false;
+  // if ((year % 4 === 0) && (year % 100 === 0) || (year % 400 === 0)) {
+  //   return true;
+  // }
+  // return false;
+  return ((year % 4 === 0) && (year % 100 === 0) || (year % 400 === 0));
 };
 
-console.log(leapYear(1997)); //not a leap year 
-console.log(leapYear(1996)); //is a leap year
-console.log(leapYear(1900)); //not a leap year
-console.log(leapYear(2000)); //is a leap year
+// console.log(leapYear(1997)); //not a leap year
+// console.log(leapYear(1996)); //is a leap year
+// console.log(leapYear(1900)); //not a leap year
+// console.log(leapYear(2000)); //is a leap year
+console.log(leapYear(prompt('Enter year')));
--- warmup/week01/leap_year/readme.md
+++ warmup/week01/leap_year/readme.md
@@ -6,16 +6,20 @@ Write a function that will take any given year and return whether it is a leap y
 Every year that is evenly divisible by 4
 Except if it is evenly divisible by 100...
 Unless it is also divisible by 400
+
 Test Data... Make sure it is working!
 
 - 1997 is not a leap year, it should return false
 - 1996 is a leap year, it should return true
 - 1900 is not a leap year, it should return false
 - 2000 is a leap year, it should return true
+
 How to structure it...
 
-We want a custom function called isLeapYear
+- We want a custom function called isLeapYear
+
 Bonus!
 
-Ask the user what number they want to test
+- Ask the user what number they want to test
+
 Watch a whole heap of information about leap years... http://www.youtube.com/watch?v=xX96xng7sAE
