91cbf2383583b8dba1d6de4c696b2383e54c9486
Paulina <p.kijowska@gmail.com>
2019-06-30 13:57:23 +1000

    created names and indexes

--- paulina_kijowska/week01/MTI /js/mti.js
+++ paulina_kijowska/week01/MTI /js/mti.js
@@ -9,24 +9,40 @@
 
 const lines = [
 
-{name: "Nline", stops: ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"] },
+{name: "Nline", stops: ["Times Square", "34th", "28th", "23rd", "Union Square", "8th"]},
 {name:"Lline", stops: ["8th", "6th", "Union Square", "3rd", "1st"] },
 {name: "Sline", stops: ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]}
 ];
 
-var planTrip = function(lineGetOn, stopGetOn, lineGetOff, stopGetOff ){
 
+
+var planTrip = function(lineGetOn, stopGetOn, lineGetOff, stopGetOff ){
+//console.log the name of the current line we should get on at
 for (var i=0; i<=2; i++ ) {
 if (lineGetOn === lines[i].name){
   console.log(lines[i].name);
-
+  //console.log where is the stop we should get on at in our array
+  console.log(lines[i].stops.indexOf(stopGetOn));
 }
 
-}
-console.log(lines[0].stops);
-console.log(lines[0].stops);
-console.log(lines[0].stops);
-console.log(lines[0].stops);
 
-};
-planTrip('Sline', 'Times Square', '6', '33rd');
+//console.log which line we should get off at
+if (lineGetOff === lines[i].name){
+  console.log(lines[i].name);
+//console.log which stop we should get off at in our array (which number in the array?)
+  console.log(lines[i].stops.indexOf(stopGetOff));
+  }
+
+}
+}
+planTrip('Nline', 'Times Square', 'Sline', '33rd');
+//based on the name of the stop we iteriate through the loop to console.log the names of the stops we will pass
+
+
+
+// var accounts= "";
+// var b = lines[i].stops.indexOf(stopGetOn);
+// for (var i = 0; i < lines[b].stops.length; i++) {
+//
+//
+// console.log("You need to pass following stops "+ accounts);
