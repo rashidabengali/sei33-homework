710189338c810deff351479e9bca46a73a06be9b
Meo So <meowsoso@gmail.com>
2019-06-30 17:27:42 +1000

    MTA revise 30June19

--- meo_so/week01/MTA/js/MTA.js
+++ meo_so/week01/MTA/js/MTA.js
@@ -1,4 +1,4 @@
-console.log("This is MTA")
+console.log("This is MTA");
 
 //create object holding all train lines in array
 const trainLines = {
@@ -7,74 +7,88 @@ const trainLines = {
   '6': ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place']
 };
 
-// function to define train lines for parameter
+// function to define train lines 
+// input: train line name (string)
+// output: clone of train line (array)
 const getLine = function (line) {
-    const lineToCalculate = Array.from(trainLines[line]);
-    return lineToCalculate;
-}
+    return Array.from(trainLines[line]);
+};
 
 // function to get segment of train line 
+// input: train line (array), starting station name (string), destination station on same line (string)
+// output: stations passed on the train line (array)
 const lineSeg = function (line, start, finish) {
-    const changeIndex = line.indexOf(finish);
+    const endIndex = line.indexOf(finish);
     const startIndex = line.indexOf(start);
-        if (startIndex < changeIndex) {
-            return line.splice(startIndex+1, (changeIndex - startIndex));
-        } else {
-            return line.splice(changeIndex, (startIndex - changeIndex)).reverse();
-        };
-};
-// function to run planTrip
-const planTrip = function (startLine, startStation, finalLine, finalStation) {
-    if (getLine(startLine).indexOf(startStation) < 0 || getLine(finalLine).indexOf(finalStation) < 0) {
-        return console.log('Stations do not exist on the train line.');
-    } else if (startLine === finalLine) {
-        const stopsPassed  = lineSeg (getLine(startLine), startStation, finalStation);
-        console.log(`You must travel through the following stops on the ${ startLine } line: ${ stopsPassed.join(', ') }.\n${ stopsPassed.length} stops in total.`);
-        console.log('4', trainLines.N);
-    } else {
-        const firstTrip = lineSeg(getLine(startLine), startStation, 'Union Square'); //trip from start to US station
-        console.log(`You must travel through the following stops on the N line: ${ firstTrip.join(', ') }.\nChange at Union Square.`);
-        const secTrip = lineSeg(getLine(finalLine), 'Union Square', finalStation);
-        console.log(`Your journey continues through the following stops: ${ secTrip.join(', ')}. \n${ (firstTrip.length + secTrip.length) } stops in total.`);
+    // using station index to calculate the direction of the ride
+    if (startIndex < endIndex) {
+        return line.splice(startIndex+1, (endIndex - startIndex));
     };
+    return line.splice(endIndex, (startIndex - endIndex)).reverse();
 };
-planTrip('N', '23rd', 'N', '8th');
-planTrip('6', '35th', 'N', 'Union Square'); //wrong name
-planTrip('6', 'Grand Central', '6', 'Astor Place'); // from 6line one end to another
-planTrip('N', '23rd', 'L', '8th'); // from N to L
-planTrip('N', 'Times Square', '6', 'Grand Central'); //'longest from N to 6'
 
-// planTrip('N', '23rd', 'L', '3rd');
-// const toUs = function (line, startStop) {
-// const changeStopIndex  = line.indexOf('US');
-// if (line.indexOf(startStop) < changeStopIndex) {
-//     for (let i = line.indexOf(startStop)-1; i <= changeStopIndex; i++) {
-//         stopsRouteA.push(line[i]);
-//     };
-// };
-//     for (let i = line.indexOf(startStop)+1; i >= changeStopIndex; i--) {
-//         stopsRouteA.push(line[i]);
-// };
-// console.log(`You must travel through the following stops on the ${ line } line: ${ stopsRouteA }. \nChange at Union Square.`);
-// }
+const tripMessage1 = function (line, stationsPassed) {
+    return `You must travel through the following stops on the ${ line } line: ${ stationsPassed.join(', ') }.`;
+}
 
-// const fromUs = function (line, finalStop) {
-// const changeStopIndex = line.indexOf('US');
-// if (line.indexOf(finalStop) < changeStopIndex) {
-//     for (let i = changeStopIndex; i > line.indexOf(finalStop); i--){
-//         stopsRouteB.push(line[i]);
-//     } 
-// }
-//     for (let i = changeStopIndex; i < line.indexOf(finalStop); i++) {
-//         stopsRouteB.push(line[i]);
-// };
-// console.log(`Your journey continues through the following stops: ${ stopsRouteB }.\n${ (stopsRouteA.length + stopsRouteB.length) } stops in total.`);
-// };
-// const planTrip = function (startLine, startStop, finalLine, finalStop) {
-//     stopsRouteA = [];
-//     stopsRouteB = [];
-//     toUs(startLine, startStop);
-//     fromUs(finalLine, finalStop);
-// }
-// planTrip(nLine, '34', six, '33');
-// planTrip(six, 'GC', lLine, '8');
+const checkInput = function (startLine, startStation, finalLine, finalStation) {
+    const errorMessage = [];
+    if (!Object.keys(trainLines).includes(startLine) || !Object.keys(trainLines).includes(finalLine)) {
+        errorMessage.push('Invalid lines');
+    } else if (getLine(startLine).indexOf(startStation) < 0 || getLine(finalLine).indexOf(finalStation) < 0) {
+        errorMessage.push('Invalid station names');
+    };
+    return errorMessage;
+};
+
+// function to run planTrip
+// input: first train line name (string), firt station name (string), destination train line (string), 
+// destination station name (string).
+// first check the input train line and station are valid. 
+// output: error message if input are invalid. exit function. 
+// then check if the trip involve only one train line, 
+// output: print details of one train line trip and total stops. exit function. 
+// if trips involve two train line: 
+// output: print first trip details then print second trip details. Print total stops involed. exit function.
+const planTrip = function (startLine, startStation, finalLine, finalStation) {
+    // input checking
+    const errorMessage = checkInput(startLine, startStation, finalLine, finalStation);
+    if (errorMessage.length > 0) {
+        return console.log(errorMessage);
+    // when trip happen on one line only
+    } else if (startLine === finalLine) {
+        const stopsPassed  = lineSeg(getLine(startLine), startStation, finalStation);
+        return console.log(tripMessage1(startLine, stopsPassed) + `\n${ stopsPassed.length} stops in total.`);
+    };
+    // stations travelled on starting line
+    const firstTrip = lineSeg(getLine(startLine), startStation, 'Union Square'); //trip from start to US station
+    // only show trip on line when array is not empty
+    console.log((firstTrip.length > 0) ? (tripMessage1(startLine, firstTrip) + `\nChange at Union Square.`) : `\nChange at Union Square.`);
+    // stations travelled on finish line
+    const secTrip = lineSeg(getLine(finalLine), 'Union Square', finalStation); //trip from US to final stop.
+        console.log(tripMessage1(finalLine, secTrip) + `\n${ (firstTrip.length + secTrip.length) } stops in total.`);
+    };
+
+let run = function () {
+    let promprom= true;
+    if (promprom){
+        planTrip(prompt(`starting train line.`), prompt('starting station'), prompt('destination train line'), prompt('destination station'));
+    }else{
+        console.log('wrong line name');
+        planTrip('LD', '23rd', 'N', '8th');
+        console.log('\none trip on N 23rd to N 8th');
+        planTrip('N', '23rd', 'N', '8th');
+        console.log('\nwrong name');
+        planTrip('6', '35th', 'N', 'Union Square'); //wrong name
+        console.log('\nfrom 6line Grand Central one end to another , Astor Place');
+        planTrip('6', 'Grand Central', '6', 'Astor Place'); // from 6line one end to another
+        console.log('\nfrom N 8th to L 1st');
+        planTrip('N', '8th', 'L', '1st'); // from N to L
+        console.log('\nlongest from N Times Square to 6 Grand Central');
+        planTrip('N', 'Times Square', '6', 'Grand Central'); //'longest from N to 6'
+        console.log('\nstart from N union square to 6 Astor Place');
+        planTrip('N', 'Union Square', '6', 'Astor Place');
+                
+    }
+}
+run();
