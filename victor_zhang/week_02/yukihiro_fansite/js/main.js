8005814996fe61f0f9f4a44b17ff7d331687498d
Victor Zhang <victorzw895@gmail.com>
2019-07-05 09:06:24 +1000

    catwalk, fixes

---
+++ victor_zhang/week_02/yukihiro_fansite/js/main.js
@@ -0,0 +1,23 @@
+
+const $topBar = $('header');
+
+$topBar.hide();
+
+$topBar.slideDown(1500);
+
+
+// topBar.css('position', 'absolute');
+// topBar.css('top','0px');
+
+// $topBar.css({
+//   'position':'absolute',
+// })
+
+// $topBar.animate('top','100px');
+
+// topBar.animate({
+//
+// })
+// topBar.css({
+//   'visibility':'none'
+// });
