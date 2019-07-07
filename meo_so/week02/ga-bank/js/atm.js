85419743b63cb84d6d62c6c600bf4d14d393bc80
Meo So <meowsoso@gmail.com>
2019-07-07 19:59:45 +1000

    homework ATM week02

--- meo_so/week02/ga-bank/js/atm.js
+++ meo_so/week02/ga-bank/js/atm.js
@@ -1,5 +1,5 @@
 console.log('bank atm');
-
+//object holding accounts and bank calculation functions
 const bank = {
     acc001: {
         name: 'Alex Wong',
@@ -7,77 +7,77 @@ const bank = {
         savings: 0
     },
     acc002: {
-        name: 'Alex Wong',
+        name: 'Lopen Hong',
         checking: 0,
         savings: 0
     },
-    
-}
-
-//update account balance, return new balance
-const deposit = function (accName ='acc001', accType, amount) {
-    bank[accName][accType] += parseFloat(amount);
-    return console.log(`${ accType }, ${bank[accName][accType]}`);
-;}
-
-const withdraw = function (accName = 'acc001', accType, amount) {
-    const savBal = bank[accName]['savings'];
-    console.log(savBal);
-    const checkBal = bank[accName]['checking'];
-    console.log(checkBal);
-    // if amount is less than whole account
-    if (amount > (savBal + checkBal) ) {
-        return console.log('not enough funding, ignore');
+    //function handling deposit when deposit button is clicked
+    deposit: function (accName ='acc001', accType, amount) {
+        bank[accName][accType] += parseFloat(amount);
+        return console.log(`${ accType }, ${bank[accName][accType]}`);
+    },
+    //function handling withdraw
+    withdraw: function (accName = 'acc001', accType, amount) {
+        const savBal = bank[accName]['savings'];
+        console.log(savBal);
+        const checkBal = bank[accName]['checking'];
+        console.log(checkBal);
+        // if amount is less than whole account
+        if (amount > (savBal + checkBal) ) {
+            return console.log('not enough funding, ignore');
         // when withdraw from savings with not enough money in savings
-    } else if (accType === 'savings' && amount > savBal) {
-        deposit(accName, accType, savBal*-1);
-        const remain = amount - savBal;
-        deposit(accName, 'checking', remain*-1);
+        } else if (accType === 'savings' && amount > savBal) {
+            bank.deposit(accName, accType, savBal*-1);
+            const remain = amount - savBal;
+            bank.deposit(accName, 'checking', remain*-1);
         // when withdraw from check with not enough money in check
-    } else if (accType === 'checking' && amount > checkBal ) {
-        deposit(accName, accType, checkBal*-1);
-        const remain2 = amount - checkBal;
-        deposit(accName, 'savings', remain2*-1);
-    } else {
-        deposit(accName, accType, amount*-1);
-    };
-};
-//Object to hold functions connecting DOM
-const jQueryDom = {
-
+        } else if (accType === 'checking' && amount > checkBal ) {
+            bank.deposit(accName, accType, checkBal*-1);
+            const remain2 = amount - checkBal;
+            bank.deposit(accName, 'savings', remain2*-1);
+        } else {
+            bank.deposit(accName, accType, amount*-1);
+        };
+    },
 }
-
-const changeColor = function () {
-    if ($('#savings-balance').html() === '$0') {
-        $('#savings-balance').addClass('zero');
-    } else {
-        $('#savings-balance').removeClass('zero');
-    };
-    if ($('#checking-balance').html() === '$0') {
+// object holding DOM changing functions
+const jQueryDom = {
+    //check acc bal and turn red when bal is $0
+    changeColor: function () {
+        if ($('#savings-balance').html() === '$0') {
+            $('#savings-balance').addClass('zero');
+        } else {
+            $('#savings-balance').removeClass('zero');
+        };
+        if ($('#checking-balance').html() === '$0') {
             $('#checking-balance').addClass('zero');
         } else {
             $('#checking-balance').removeClass('zero');
-    };
+        };
+    },
+    //trigger functions to update bank balance 
+    updateBal: function () {
+        const accType = $(this).closest('div').attr('id');
+        const amount = $(this).siblings('[type="text"]').val();
+        if (amount <= 0 || isNaN(amount)) {
+            return;
+        };
+        if ($(this).attr('value') === "Deposit") {
+            bank.deposit('acc001', accType, amount);
+        } else {
+            bank.withdraw('acc001', accType, amount);       
+        };    
+    },
+    //update display of balance on page
+    showBal: function () {
+        checkingBal = bank['acc001']['checking'];
+        savingsBal = bank['acc001']['savings'];
+        $('#checking-balance').text(`$${ checkingBal }`);
+        $('#savings-balance').text(`$${ savingsBal }`);
+    },
 };
-
-const updateBal = function () {
-    const accType = $(this).closest('div').attr('id');
-    const amount = $(this).siblings('[type="text"]').val();
-    if ($(this).attr('value') === "Deposit") {
-        const accBal = deposit('acc001', accType, amount);
-    } else {
-        withdraw('acc001', accType, amount);       
-    };    
-}
-
-const showBal = function () {
-    checkingBal = bank['acc001']['checking'];
-    savingsBal = bank['acc001']['savings'];
-    $('#checking-balance').text(`$${ checkingBal }`);
-    $('#savings-balance').text(`$${ savingsBal }`);
-};
-
-
-$('[type="button"]').on('click', updateBal)
-                    .on('click', showBal)
-                    .on('click', changeColor);
+//event listener 
+$('[type="button"]').on('click', jQueryDom.inputCheck)
+                    .on('click', jQueryDom.updateBal)
+                    .on('click', jQueryDom.showBal)
+                    .on('click', jQueryDom.changeColor);
