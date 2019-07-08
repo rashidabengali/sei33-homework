771856979183064ef160a183ee492d6ffe45e26a
Paulina <p.kijowska@gmail.com>
2019-07-08 18:32:53 +1000

    second version

--- paulina_kijowska/projects/tic-tac-toe/js/tictactoe.js
+++ paulina_kijowska/projects/tic-tac-toe/js/tictactoe.js
@@ -1,89 +1,60 @@
-// $(document).ready(function () {
-//
-//
-// //how much money the user put in the input bit
-// const $inputcheck = $("#checking-amount").val();
-//
-// const $inputsav = $("#savings-amount").val()
-//
-// ///how much money inside
-// const $totcheck = $("div #checking-balance").text();
-// const $totsav = $("div #savings-balance").text();
-// console.log($totcheck);
-// console.log($totsav);
-//
-//
-//
-//       $('#checking-deposit').on('click', function() {
-//          // var number = parseInt($totcheck);
-//
-//          var deposit = + $("#checking-amount").val();
-//
-//  var $tot = $("#checking-balance").html("$"+value);
-//  // var $toti = $( "#checking-balance" ).append("<p>$toti<p>");
-//  const totally = 0;
-//
-//
-//
-//
-//   });
-//
-//   $(function(lol) {
-//         $('#savings-deposit').on('click', function() {
-//            // var number = parseInt($totcheck);
-//            var value = $("#savings-amount").val();
-//    var $tot = $("#savings-balance").html("$"+value);
-//
-//         });
-//     });
-//
-//
-//
-//
-// });
-// // $("#checking-deposit").on("click", function){ $("#checking-balance").replaceWith($inputcheck)});
+
+
+
+
 
 $(document).ready(function () {
-  $('#checking-deposit').on('click', function () {
-    const deposit = + $('#checking-amount').val();
-    const currentBalance = + $('#checking-balance').text().slice(1);
-    const newBalance = currentBalance + deposit;
-    $('#checking-balance').text('$' + newBalance);
-  });
 
-  $('#checking-withdraw').on('click', function () {
-    const deposit = + $('#checking-amount').val();
-    const currentBalance = + $('#checking-balance').text().slice(1);
+// prompt("What is your name?");
+//
+// alert("Pick your picture");
 
-    const otherBalance = + $('#savings-balance').text().slice(1);
-    const totalBalance = currentBalance + otherBalance;
+// $(".one").click(function(){
+//   $("td:first").addClass("pic");
 
-    const newBalance = currentBalance - deposit;
+$("#image-one").click(function(){
 
-    if (newBalance >= 0) {
-      $('#checking-balance').text('$' + newBalance);
-    } else if (deposit <= totalBalance) {
-      $('#checking-balance').text('$0');
-      $('#savings-balance').text('$' + (otherBalance + newBalance));
-    }
-  });
+console.log("you clicked the headgehog");
 
-  $('#savings-deposit').on('click', function () {
-    const deposit = + $('#savings-amount').val();
-    const currentBalance = + $('#savings-balance').text().slice(1);
-    const newBalance = currentBalance + deposit;
-    $('#savings-balance').text('$' + newBalance);
-  });
 
-  $('#savings-withdraw').on('click', function () {
-    const deposit = + $('#savings-amount').val();
-    const currentBalance = + $('#savings-balance').text().slice(1);
-    const newBalance = currentBalance - deposit;
 
-    if (newBalance >= 0) {
-      $('#savings-balance').text('$' + newBalance);
-    }
 
-  });
+
+});
+
+//
+// var firstSrc = "https://media1.giphy.com/media/3xz2BCohVTd7h2Kvfi/200.webp?cid=790b76115d22c46545414a44552b6e55&rid=200.webp";
+//    var secondSrc = "https://media0.giphy.com/media/23xN9cYQSKwFy/200w.webp?cid=790b76115d22c4df78505038595e6040&rid=200w.webp";
+//    alert(firstSrc == secondSrc);
+  $('#one').click(function(){
+          // $('.one').addClass(".image-two");
+
+
+          const img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+          img.attr('src', "https://media1.giphy.com/media/3xz2BCohVTd7h2Kvfi/200.webp?cid=790b76115d22c46545414a44552b6e55&rid=200.webp");
+          img.appendTo('#one');
+
+      });
+
+      $('#two').click(function(){
+              // $('.one').addClass(".image-two");
+
+
+              const img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+              img.attr('src', "https://media1.giphy.com/media/3xz2BCohVTd7h2Kvfi/200.webp?cid=790b76115d22c46545414a44552b6e55&rid=200.webp");
+              img.appendTo('#two');
+
+          });
+
+          $('#three').click(function(){
+                  // $('.one').addClass(".image-two");
+
+
+                  const img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+                  img.attr('src', "https://media1.giphy.com/media/3xz2BCohVTd7h2Kvfi/200.webp?cid=790b76115d22c46545414a44552b6e55&rid=200.webp");
+                  img.appendTo('#three');
+
+              });
+
 
 });
