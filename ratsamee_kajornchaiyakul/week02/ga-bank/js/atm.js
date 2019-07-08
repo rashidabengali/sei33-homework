97d037dce5e8bb2b3b6af105f8458ca7548d3f01
Ratsamee <jayin@hotmail.com>
2019-07-08 10:59:12 +1000

    week02 day05 separate account file

--- ratsamee_kajornchaiyakul/week02/ga-bank/js/atm.js
+++ ratsamee_kajornchaiyakul/week02/ga-bank/js/atm.js
@@ -1,30 +1,5 @@
 const CHECKING_ACCOUNT = 'checking_account';
 const SAVING_ACCOUNT = 'saving_account';
-
-const account = function(accType){
-    return {
-        accountType: accType,
-        balance: 0,
-        deposit: function(depositMoney){
-            this.balance += depositMoney;
-        },
-        withdraw: function(withdrawMoney, accountLink){
-            const balanceAfterWithdraw = this.balance - withdrawMoney;
-            if (balanceAfterWithdraw > -1){
-                this.balance = balanceAfterWithdraw;
-                return true;
-            }else if(accountLink) {
-                const resultWithdraw = accountLink.withdraw(balanceAfterWithdraw * -1);
-                if (resultWithdraw){
-                    this.balance = 0;
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-}
-
 let checkingAccount;
 let savingAccount;
 let $inputCheckingAmount;
