7540ef47e7af68a61827f398eaa470cc63a8f14f
Lucy Ogden-Doyle <lucyod10@gmail.com>
2019-07-09 10:47:26 +1000

    stroke, win and draw

--- lucy_ogdenDoyle/tictactoe/js/main.js
+++ lucy_ogdenDoyle/tictactoe/js/main.js
@@ -20,7 +20,7 @@ const game = {
     }
   },
 
-  matchCheck: function (one, two, three) {
+  matchCheck: function (one, two, three, winId) {
     one = Number(one) - 1;
     two = Number(two) - 1;
     three = Number(three) - 1;
@@ -29,36 +29,54 @@ const game = {
     if (this.gameState[one] === this.gameState[two]
         && this.gameState[two] === this.gameState[three]
         && this.gameState[one] != "" && this.gameState[two] != "" && this.gameState[three] != "" ) {
-          $('body').append("<div id='winner'></div>");
-          $('#winner').text(`the winner is ${this.gameState[one]}`);
+          //add winner message
+          $("#win").show();
+          $('#win p').text(`the winner is ${this.gameState[one]}`);
+          //add stroke
+          $(".board").append("<div id='stroke'></div>");
+          $("#stroke").addClass(winId);
           return true;
         }
-    else if ("x" == "x") {
-      //TODO check if board is full
-    }
     else {
       return false;
-
     }
   },
 
   checker: function () {
     //horizontal match
-    if (this.matchCheck(1, 2, 3) ||
-    this.matchCheck(4, 5, 6)||
-    this.matchCheck(7, 8, 9)||
+    if (this.matchCheck(1, 2, 3, "stroke strokeHoriz winHorizontal1") ||
+    this.matchCheck(4, 5, 6, "stroke strokeHoriz winHorizontal2")||
+    this.matchCheck(7, 8, 9, "stroke strokeHoriz winHorizontal3")||
     //vertical match
-    this.matchCheck(1, 4, 7)||
-    this.matchCheck(2, 5, 8)||
-    this.matchCheck(3, 6, 9)||
+    this.matchCheck(1, 4, 7, "stroke strokeVert winVertical1")||
+    this.matchCheck(2, 5, 8, "stroke strokeVert winVertical2")||
+    this.matchCheck(3, 6, 9, "stroke strokeVert winVertical3")||
     //diagonal match
-    this.matchCheck(1, 5, 9)||
-    this.matchCheck(3, 5, 7)) {
-      this.match();
+    this.matchCheck(1, 5, 9, "stroke winDiagonal1")||
+    this.matchCheck(3, 5, 7, "stroke winDiagonal2")) {
+    }
+    else if (this.checkBoardFull() === true) {
+      //if board true, call draw.
+      //add winner message
+      $("#win").show();
+      $('#win p').text(`Draw!`);
     }
     else {
       this.nextTurn();
     }
+  },
+
+  checkBoardFull: function () {
+    //TODO check if board is full
+    for (let i=0; i < game.gameState.length; i++) {
+      //for each item on the board, check if any are empty.
+      if (game.gameState[i] === "") {
+        console.log("gamestate " + game.gameState[i]);
+        //if board not full, move along
+        return false;
+      }
+    }//loop
+    return true;
   },
 
   nextTurn: function () {
@@ -71,6 +89,10 @@ const game = {
   },
 
   match: function () {
+    //on new game
+    //remove stroke
+    $(".stroke").remove();
+    //reset gameState
     for (let i=0; i < 9; i++) {
       this.gameState[i] = "";
       render();
@@ -95,8 +117,6 @@ addClicker(".seven", "7");
 addClicker(".eight", "8");
 addClicker(".nine", "9");
 
-render();
-
 function render () {
   //render the board
   $(".one").text(game.gameState[0]);
@@ -109,3 +129,12 @@ function render () {
   $(".eight").text(game.gameState[7]);
   $(".nine").text(game.gameState[8]);
 }
+
+$("#win button").on("click", function () {
+  game.match();
+  $("#win").hide();
+})
+
+// on start
+render();
+$("#win").hide();
