cb8c19d2903dd1230b2ab91eed916995f902f744
AJ <aj.tauber98@gmail.com>
2019-07-16 09:00:56 +1000

    Week04Day01HW

--- alec_tauber/week_01/arrays/js/ArraysHW.js
+++ alec_tauber/week_01/arrays/js/ArraysHW.js
@@ -163,45 +163,40 @@ console.log(findLongestWord(['hello','lol', 'deaths', 'Bob'], 5));
 //
 
 
-var wordLetters = ['C', 'A', 'T', 'S'];
-var guessedLetters = ['_', '_', '_', '_'];
+// var wordLetters = ['C', 'A', 'T', 'S'];
+// var guessedLetters = ['_', '_', '_', '_'];
+//
+// const guessLetter = function(letter){
+//   var goodGuess = false;
+//
+//
 
-const guessLetter = function(letter){ //
+var wordLetters     = ['G', 'O', 'A', 'T'];
+var guessedLetters  = ['_', '_', '_', '_'];
 
+function guessLetter(letter) {
+    var goodGuess = false;
+    var moreToGuess = false;
+    for (var i = 0; i < wordLetters.length; i++) {
+        if (wordLetters[i] == letter) {
+            guessedLetters[i] = letter;
+            goodGuess = true;
+        }
+        if (guessedLetters[i] == '_') {
+            moreToGuess = true;
+        }
+    }
+    if (goodGuess) {
+        console.log('Yay, you found a letter!');
+        console.log(guessedLetters.join(''));
+        if (!moreToGuess) {
+            console.log('YOU WON!');
+        }
+    } else {
+        console.log('Oh noes, thats not right!');
+    }
 }
-// var wordLetters     = ['G', 'O', 'A', 'T'];
-// var guessedLetters  = ['_', '_', '_', '_'];
-//
-// function guessLetter(letter) {
-//     var goodGuess = false;
-//     var moreToGuess = false;
-//     for (var i = 0; i < wordLetters.length; i++) {
-//         if (wordLetters[i] == letter) {
-//             guessedLetters[i] = letter;
-//             goodGuess = true;
-//         }
-//         if (guessedLetters[i] == '_') {
-//             moreToGuess = true;
-//         }
-//     }
-//     if (goodGuess) {
-//         console.log('Yay, you found a letter!');
-//         console.log(guessedLetters.join(''));
-//         if (!moreToGuess) {
-//             console.log('YOU WON!');
-//         }
-//     } else {
-//         console.log('Oh noes, thats not right!');
-//     }
-// }
-//
-// guessLetter('G');
-// guessLetter('I');
-// guessLetter('O');
-// guessLetter('A');
-// guessLetter('T');
-//
-//
+guessLetter('');
 //
 
 
