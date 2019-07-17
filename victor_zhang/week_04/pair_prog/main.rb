c226292b765be2c4bd27f535500d40f1eb2676c1
Victor Zhang <victorzw895@gmail.com>
2019-07-17 21:33:18 +1000

    week04 day3 - books, pair-prog

--- victor_zhang/week_04/pair_prog/main.rb
+++ victor_zhang/week_04/pair_prog/main.rb
@@ -7,10 +7,26 @@ get '/' do
   erb :home
 end
 
-get 'pass' do
-  
+get '/pass' do
+  erb :pass
 end
 
-get 'write_test' do
+get '/refactor' do
+  erb :refactor
+end
 
+get '/do_refactor' do
+  erb :do_refactor
+end
+
+get '/new_feature' do
+  erb :new_feature
+end
+
+get '/write_code' do
+  erb :write_code
+end
+
+get '/write_test' do
+  erb :write_test
 end
