f3f508e7ff0623fcaa10df4f2c55863674e3ead6
AJ <aj.tauber98@gmail.com>
2019-07-19 11:41:47 +1000

    Week04Day05HWUpdated

--- alec_tauber/week_04/crud/main.rb
+++ alec_tauber/week_04/crud/main.rb
@@ -35,12 +35,22 @@ end
 # EDIT -- show the form to edit a particular existing butterfly
 get '/games/:id/edit' do
   @games = query_db "SELECT * FROM games WHERE id = #{params[:id]}"
-  @games = games.first
+  @games = @games.first
   erb :games_edit
 end
 
+# UPDATE -- update the database with new information for some particular butterfly
+post '/games/:id' do
+  query = "UPDATE games SET title='#{ params[:title] }', genre='#{ params[:genre] }', platform='#{params[:platform]}', image='#{ params[:image] }' WHERE id = #{ params[:id] }"
+  query_db query
+  redirect to("/games/#{ params[:id] }")
+end
 
-
+# DESTROY -- delete the butterfly with some butterfly ID from the database
+get '/games/:id/delete' do
+  query_db "DELETE FROM games WHERE id = #{ params[:id] }"
+  redirect to('/games')
+end
 
 def query_db(sql_statement)
   puts sql_statement # Optional but nice for debugging
