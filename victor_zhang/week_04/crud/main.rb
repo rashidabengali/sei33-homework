502beff799c807c2b3a6f629b772cbe75cc03843
Victor Zhang <victorzw895@gmail.com>
2019-07-22 09:08:15 +1000

    homework week4 day5 - crud

--- victor_zhang/week_04/crud/main.rb
+++ victor_zhang/week_04/crud/main.rb
@@ -1,19 +1,42 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'active_record'
 require 'pry'
 
 
+# Establish connection whilst using Sinatra
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'database.sqlite3'
+)
+
+# Optional bonus
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+## Create Classes
+class Blog_notes < ActiveRecord::Base
+end
+
+class Project < ActiveRecord::Base
+end
+
+class Version < ActiveRecord::Base
+end
+
+
+## Blog Entries ###########################################################
 get '/' do
   erb :home
 end
 
-# Show list of all entries
+# List of all entries
 get '/blog_entries' do
-  @entries = query_db "SELECT * FROM blog_notes"
+  @entries = Blog_notes.all
   erb :blog_entries
 end
 
+# NEW
 get '/blog_entry/new' do
   erb :blog_entry_new
 end
@@ -21,53 +44,134 @@ end
 # SHOW selected entry from ALL ENTRIES
 get '/blog_entry/:id' do
   # binding.pry
-  @entry = query_db "SELECT * FROM blog_notes WHERE id = #{ params[:id] }"
+  @entry = Blog_notes.find params[:id]
 
-  @entry = @entry.first
   # binding.pry
   erb :blog_entry
 end
 
+# CREATE
 post '/blog_entry' do
-  query = "INSERT INTO blog_notes (title, entry_date, entry, image, tags) VALUES ('#{ params[:title] }', '#{ params[:entry_date] }', '#{ params[:entry] }', '#{ params[:image] }', '#{ params[:tags] }')"
-  query_db query
+  entry = Blog_notes.new
+  entry.title = params[:title]
+  entry.entry_date = params[:entry_date]
+  entry.entry = params[:entry]
+  entry.image = params[:image]
+  entry.tags = params[:tags]
+  entry.save
 
-  redirect to('/blog_entries')
+  redirect to("/blog_entry/#{ entry.id }")
   # redirect to("/blog_entry/#{ params[:id] }") #cant because the id has not been sent as a params
 end
 
 # EDIT
 get '/blog_entry/:id/edit' do
-  @entry = query_db "SELECT * FROM blog_notes WHERE id = #{ params[:id] }"
-  @entry = @entry.first
+  @entry = Blog_notes.find params[:id]
 
   erb :blog_entry_edit
 end
 
 # DELETE
 get '/blog_entry/:id/delete' do
-  query_db "DELETE FROM blog_notes WHERE id = #{ params[:id] }"
+  entry = Blog_notes.find params[:id]
+  entry.destroy
   redirect to('/blog_entries')
 end
 
+# UPDATE
 post '/blog_entry/:id' do
 
-  query = "UPDATE blog_notes SET title='#{ params[:title] }', entry_date='#{ params[:entry_date] }', entry='#{ params[:entry] }', image='#{ params[:image] }', tags='#{ params[:tags] }' WHERE id = #{ params[:id] }"
-  query_db query
+  entry = Blog_notes.find params[:id]
+  entry.title = params[:title]
+  entry.entry_date = params[:entry_date]
+  entry.entry = params[:entry]
+  entry.image = params[:image]
+  entry.tags = params[:tags]
+  entry.save
   redirect to("/blog_entry/#{ params[:id] }")
 end
 
+## Projects ################################################################
 
-def query_db sql_statement
-  puts sql_statement # For debugging
+# INDEX
+get '/projects/' do
+  @projects = Project.all
+  @versions = Version.all
+  # binding.pry
+  erb :projects
+end
 
-  # connect to database
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true #instead of get a return array of array, get an array of hash
+# CREATE NEW
+get '/project/new' do
+  erb :project_new
+end
 
-  # run sql statement from the database
-  results = db.execute sql_statement
+post '/project/' do
+  project = Project.new
+  project.name = params[:name]
+  project.entry_date = params[:entry_date]
+  project.link = params[:link]
+  project.tags = params[:tags]
+  project.save
+  version = Version.new
+  version.project_name = params[:name]
+  version.version = params[:version]
+  version.image = params[:image]
+  version.save
+  redirect to("/project/#{ project.id }/#{ version.id }")
+end
 
-  db.close
-  results # implicit return
+get '/project/:id/:vid/delete' do
+  version = Version.find params[:vid]
+  version.destroy
+  redirect to('/projects/')
+end
+
+# NEW Version
+get '/project/:id/update/' do
+  # binding.pry
+  @project = Project.find params[:id]
+  erb :project_update
+end
+
+get '/project/:id/:vid/edit' do
+  @project = Project.find params[:id]
+  @version = Version.find params[:vid]
+  erb :project_edit
+end
+
+# SHOW
+get '/project/:id/:vid' do
+  # binding.pry
+  @project = Project.find params[:id]
+  @version = Version.find params[:vid]
+  erb :project_view
+end
+
+post '/project/:id/version-update/' do
+  project = Project.find params[:id]
+  version = Version.new
+  # binding.pry
+  version.project_name = params[:name]
+  version.version = params[:version]
+  version.image = params[:image]
+  version.save
+  redirect to("/project/#{ project.id }/#{ version.id }")
+end
+
+# EDIT
+post '/project/:id' do
+  project = Project.find params[:id]
+  project.name = params[:name]
+  project.version = params[:version]
+  project.entry_date = params[:entry_date]
+  project.link = params[:link]
+  project.image = params[:image]
+  project.tags = params[:tags]
+  project.save
+  redirect to("/project/#{ project.id }")
+end
+
+after do
+  ActiveRecord::Base.connection.close
 end
