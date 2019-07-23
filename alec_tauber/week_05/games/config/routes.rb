8d18a87b800120d6af19cc0909bf7a3ca1cc1572
AJ <aj.tauber98@gmail.com>
2019-07-24 09:09:31 +1000

    Week05Day02HW

--- alec_tauber/week_05/games/config/routes.rb
+++ alec_tauber/week_05/games/config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
   get '/magic' => 'magic#form'
   get '/magic/info' => 'magic#info'
-  #
+
+  root '/index' => 'pages#index'
+
+  get '/index' => 'pages#index'
+
+  get '/answers' => 'pages#answers'
+
+  get '/magic' => 'pages#magic'
   # get '/secret' => 'secret#form'
   # get '/secret/info' => 'secret#info' # Display the secret numbers
   #
