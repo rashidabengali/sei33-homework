67ed8e5de67ad5a6831bde4cc658cd3a389c49c9
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-08-08 22:19:43 +1000

    Week 07 Day 04 HW

--- nilana_pereiraDaRocha/week07/day04/js/main.js
+++ nilana_pereiraDaRocha/week07/day04/js/main.js
@@ -69,11 +69,41 @@ console.log(numbersOdds);
 
 // Get three random numbers out of numbers
 
-let numbersRandomMin = _.random(numbers, function (number) { return number.min });
-let numbersRandomMax = _.random(numbers, function (number) { return number.max });
-console.log(numbersRandomMin);
-console.log(numbersRandomMax);
+let numbersRandomOne = _.random(1, 10);
+let numbersRandomTwo = _.random(2, 10);
+let numbersRandomThree = _.random(3, 10);
 
+console.log(numbersRandomOne);
+console.log(numbersRandomTwo);
+console.log(numbersRandomThree);
+
+// okay
+
+// Here is some data that you can work with.
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var bumpyArr = ["hello", "maytag", [[[["sigmonster"]], "swizzle"]]];
+var uncompactedArr = ["hello", false, NaN, undefined, "quantom bogo-sort"];
+
+var arrToTransform = [["age", "location"], [NaN, undefined]];
+
+// Create an array of every five numbers between 30 and 101
+
+
+// Turn bumpyArr into one flat array (no nested arrays)
+
+// Remove all of the falsey elements in the uncompactedArr
+var filtered = _.filter(uncompactedArr, function (element) {
+  return element !== 0
+});
+console.log(filtered);
+
+
+// Find all of the unique elements in the following arrays - [ 1, 25, 100 ], [ 1, 14, 25 ] and 24, Infinity, -0
+
+// Find the index of the first element in numbers that is over 7 and is even
+
+// Turn arrToTransform into an object that looks like this - { age: NaN, location: undefined }
 
 
 
