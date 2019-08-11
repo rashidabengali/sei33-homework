9a50f00cd9a1ae75ba9481bd674cebedb7e39463
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-08-11 20:46:53 +1000

    Week 07 Day 05 HW

---
+++ nilana_pereiraDaRocha/week07/tic-tac-toe-react/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Game from './components/game/Game';
+import './App.css';
+
+class App extends React.Component {
+  restartGame() {
+    alert('Restart');
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <Game />
+
+
+
+      </div>
+    );
+
+  }
+
+}
+
+export default App;
