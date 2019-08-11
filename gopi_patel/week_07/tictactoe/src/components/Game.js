39d7919dd14c951225ce044e3c8a431f678fc26f
Gopi Patel <ask2gopi@gmail.com>
2019-08-11 16:19:53 +1000

    week 7 flickr search and tictactoe

---
+++ gopi_patel/week_07/tictactoe/src/components/Game.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Square from './Square';
+import Board from './Board';
+
+
+class Game extends React.Component {
+  render(){
+    return(
+      <div className = "game">
+        <div className = "game-board">
+          <Board/>
+        </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Game;
