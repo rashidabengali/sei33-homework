90327399cf9ac5d0abd9b3610e7c987afc6f2033
RashidaBengali <bengali.rashida@gmail.com>
2019-08-27 10:06:43 +1000

    happy numbers solution

--- review_sessions/main.js
+++ review_sessions/main.js
@@ -389,7 +389,7 @@ findPrime(10);
 // primeFactors(60);
 
 // Version 2
-
+/*
 const primeFactors = {
 
   range: [],
@@ -443,3 +443,63 @@ const primeFactors = {
 }
 
 console.log(primeFactors.findPrimeFactors(60));
+*/
+
+const happyNumbers = {
+
+  happyList: [],
+
+  findSquareSum: function (digit) {
+
+    let sum = 0;
+    let stringNumber = digit.toString();
+
+    for (let i = 0; i < stringNumber.length; i++) {
+      sum += +stringNumber[i] * +stringNumber[i];
+    }
+    return sum;
+  },
+
+  happyLogic: function (digit) {
+
+    let cycle = {};
+    let squareSum = digit;
+
+    console.log('checking', digit);
+
+    while (true) {
+      squareSum = this.findSquareSum(squareSum)
+
+      console.log('calculating...');
+
+      if (squareSum === 1) {
+        console.log("found a happy number", digit);
+        return true;
+      }
+
+      if (cycle[squareSum]) {
+        console.log("repeated pattern");
+        return false;
+      }
+      cycle[squareSum] = true;
+      console.log('cycle', cycle);
+      console.log('squareSum', squareSum);
+    }
+  },
+
+  printNumbers: function (num) {
+
+    let found = 0;
+
+    for (let i = 0; found < num; i++) {
+
+      if (this.happyLogic(i)) {
+        found++
+        this.happyList.push(i)
+      }
+    }
+    console.log(this.happyList);
+  }
+}
+
+happyNumbers.printNumbers(10);
